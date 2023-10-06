38d1c0a33b2dbda4f0c6faea3c93a217e969f9e6
FeLi <>
2023-10-06 19:39:02 +0200

    Proyecto Ocean en marcha

---
+++ Wwise_IDs.h
@@ -0,0 +1,124 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBMUSIC = 3402181612U;
+        static const AkUniqueID BOOM = 1761501016U;
+        static const AkUniqueID ENGINE = 268529915U;
+        static const AkUniqueID HORN = 3600847130U;
+        static const AkUniqueID LIGHTHOUSE = 2822410613U;
+        static const AkUniqueID OFF_MX = 782891988U;
+        static const AkUniqueID OFFICE = 1547173777U;
+        static const AkUniqueID ONICE = 2612351035U;
+        static const AkUniqueID PICKUP = 3978245845U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_MX = 2447410425U;
+        static const AkUniqueID REMOS = 3316443993U;
+        static const AkUniqueID TELEPORTSOUND = 2064465325U;
+        static const AkUniqueID TREASURE = 3284038070U;
+        static const AkUniqueID UFO = 1198168263U;
+        static const AkUniqueID VOICEBONUS = 32737810U;
+        static const AkUniqueID VOICEFEMALE = 4143102089U;
+        static const AkUniqueID VOICEMALE = 3472271138U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BONUS
+        {
+            static const AkUniqueID GROUP = 2356001030U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace BONUS
+
+        namespace MOUNTAIN
+        {
+            static const AkUniqueID GROUP = 513139656U;
+
+            namespace STATE
+            {
+                static const AkUniqueID IN = 1752637612U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUT = 645492555U;
+            } // namespace STATE
+        } // namespace MOUNTAIN
+
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID T1 = 1568083776U;
+                static const AkUniqueID T2 = 1568083779U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace TERRENO
+        {
+            static const AkUniqueID GROUP = 942072244U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID MUD = 712897245U;
+                static const AkUniqueID NIEVE = 642809072U;
+                static const AkUniqueID WETSTEP = 3713320225U;
+            } // namespace SWITCH
+        } // namespace TERRENO
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DISTANCIA = 2914972013U;
+        static const AkUniqueID LIGHTHOUSE = 2822410613U;
+        static const AkUniqueID PISADASCAMBIO = 1596445237U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID TREASURE = 3284038070U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID NEW = 444308101U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
